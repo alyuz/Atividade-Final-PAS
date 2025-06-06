c253a4e66076793e4194c4b67f90c24f4e090ae0
Alice Beserra <153461290+alyuz@users.noreply.github.com>
2025-06-06 10:17:23 -0300

    Create connection.py

---
+++ database/connection.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            try:
+                cls._instance = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    password="#",
+                    database="bd_projetoti",
+                    # auth_plugin='mysql_native_password'
+                )
+            except Error as e:
+                print(f"Erro ao conectar ao banco de dados: {e}")
+                cls._instance = None
+        return cls._instance
+
+    def get_connection(self):
+        return self._instance
